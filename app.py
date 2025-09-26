7e2c2b9ae01ce2edb10d3b1001035551002cc21d
Kenia <23308060610371@cetis61.edu.mx>
2025-09-26 11:09:12 -0500

    x

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 app=Flask(__name__)
-@app.route('/')
+@app.route('/') ##raiz
+
 def hola_mundo():
     return '<h1>¡Hola mundo desde Flask!</h1>'
 if __name__=='__main__':
     app.run(debug=True)
+
+def home():
+    mensaje='<h1></h1>'
