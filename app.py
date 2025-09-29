4671e0edf75cfb328d8cdf8b6fc13ce016dd8511
Kenia <23308060610371@cetis61.edu.mx>
2025-09-29 08:39:06 -0500

    x

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
-app=Flask(__name__)
-@app.route('/') ##raiz
+app = Flask(__name__)
 
+@app.route('/')  # raiz
 def hola_mundo():
     return '<h1>¡Hola mundo desde Flask!</h1>'
-if __name__=='__main__':
-    app.run(debug=True)
+if __name__ == '__main__':
+    
+@app.route('/parrafo')
+def parrafo():
+    return '''<p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>'''
 
-def home():
-    mensaje='<h1></h1>'
+@app.route('/Factorial/<v1>')
+def factorial(v1):
+    f = 1
+    for x in range(1, int(v1) + 1):
+        f *= x
+    return f"El factorial de {v1} es: {f}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
